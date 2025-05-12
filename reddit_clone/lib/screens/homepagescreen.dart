d680740eab1410298ba5918f20eca5c565ef5f08
ShubhamKharde45 <shubhamkharde45@gmail.com>
2025-05-12 18:05:36 +0530

    initial commit

---
+++ reddit_clone/lib/screens/homepagescreen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePageUI extends StatefulWidget {
+  const HomePageUI({super.key});
+
+  @override
+  State<HomePageUI> createState() => _HomePageUIState();
+}
+
+class _HomePageUIState extends State<HomePageUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              Text(
+                "data",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
